865fae621715c5ec95195a835e41ad3d7acbd64d
SayamMahajan <mahajansayam3@gmail.com>
2025-02-15 10:28:37 +0530

    update

--- backend/utils/generateJWTToken.js
+++ backend/utils/generateJWTToken.js
@@ -8,14 +8,14 @@ export const generateJWTToken = (res, patientId, email) => {
   }
 
   const token =jwt.sign( payload, process.env.JWT_SECRET, {
-    expiresIn: "7d"
+    expiresIn: "1d"
   });
 
   res.cookie('token', token, {
     httpOnly : true,
     // secure: process.env.NODE_ENV === 'production',
     // sameSite : 'strict',
-    maxAge: 7*24*60*60*1000,
+    maxAge: 1*24*60*60*1000,
   })
 
   return token;
